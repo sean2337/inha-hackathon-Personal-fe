135e370e3ccb14c263262f45e89b920dfc3fcfee
sean2337 <sean2337@naver.com>
2022-07-28 00:41:03 +0900

    page-animationt수정3

--- react-personal/src/component/Styled/animate.js
+++ react-personal/src/component/Styled/animate.js
@@ -2,32 +2,32 @@ export const animate = {
   initial: {
     opacity: 0,
     transition: `transform 1s ease`,
-    transform: `translate(-50px. 180px)`,
+    transform: `translate(-50px. 142px)`,
   },
   animate: {
     opacity: 1,
     transition: `transform 1s ease`,
-    transform: `translate(0px,180px)`,
+    transform: `translate(0px,142px)`,
   },
   exit: {
     opacity: 0,
     transition: `transform 1s ease`,
-    transform: `translate(-50px,180px)`,
+    transform: `translate(-50px,142px)`,
   },
 
   initial2: {
     opacity: 0,
     transition: `transform 1s ease`,
-    transform: `translate(-50px. 150px)`,
+    transform: `translate(-50px. 200px)`,
   },
   animate2: {
     opacity: 1,
     transition: `transform 1s ease`,
-    transform: `translate(0px,150px)`,
+    transform: `translate(0px,200px)`,
   },
   exit2: {
     opacity: 0,
     transition: `transform 1s ease`,
-    transform: `translate(-50px,150px)`,
+    transform: `translate(-50px,200px)`,
   },
 };
